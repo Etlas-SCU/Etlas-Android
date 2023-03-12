2035b42b2d0c2e5ae301a73d533b03edd6f501a5
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-03-12 20:11:28 +0200

    feat: add LaguageSelection

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import LanguageSelection from './Components/LanguageSelection/LanguageSelection'
 export default function App() {
   return (
     <View style={styles.container}>
-      <OnBoarding/>
+      <LanguageSelection />
     </View>
   );
 }
--- Components/LanguageSelection/LanguageSelection.js
+++ Components/LanguageSelection/LanguageSelection.js
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, ImageBackground, Image } from "react-native";
 import { useFonts } from 'expo-font';
+import Swiper from 'react-native-swiper'
 
 export default function LanguageSelection () {
     
@@ -14,27 +15,51 @@ export default function LanguageSelection () {
     if (!fontsLoaded)
         return null
     
-
     // if the font loaded, return the text
     return (
         <View style={styles.container}>
-            <Text style={styles.header}>Language Selection</Text>
-            <View style={styles.text_box}>
-                <Text style={styles.desc}>Is an augmented reality application that helps people to recognize monuments during their tour in anywhere in  the world by only a scan using your mobile camera.</Text>
-            </View>
-            <Text style={styles.worry}>No worries, your tour guide is here ;)</Text>
-            <View style={styles.logoContainer}>
-                <Image style={styles.logo} source={require('../../assets/on_boarding/e.png')}></Image>
-            </View>
-            <Image style={styles.wolf} source={require('../../assets/on_boarding/EG_by_Onlywolfs.png')}></Image>
-            <View style={styles.button_container}>
-                <TouchableOpacity style={[styles.sign_buttons, styles.sign_in]}>
-                    <Text style={styles.button_text}>Sign in</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={[styles.sign_buttons, styles.sign_up]}>
-                    <Text style={styles.button_text}>Sign up</Text>
-                </TouchableOpacity>
-            </View>
+            <ImageBackground source={require('../../assets/language_selection/Language_Selection.png')} resizeMode='cover' style={styles.image}>
+                <Text style={styles.header}>Language Selection</Text>
+                <View style={styles.text_box}>
+                    <Text style={styles.desc}>Please, choose your preferred language that you want to be set on the application, it can also be changed anytime.</Text>
+                </View>
+                <Swiper style={styles.wrapper} 
+                    showsButtons={true} 
+                    horizontal={false} 
+                    buttonWrapperStyle={styles.arrows}
+                    nextButton={<Image source={require('../../assets/language_selection/material-symbols_keyboard-arrow-down.png')}/>}
+                    prevButton={<Image source={require('../../assets/language_selection/material-symbols_keyboard-arrow-up.png')}/>}
+                    showsPagination={false}
+                    containerStyle={styles.swipper}
+                    bounces={true}
+                >
+                    <View style={styles.slide1}>
+                        <TouchableOpacity style={styles.continue}>
+                            <Text style={styles.button_text}>العربــية</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.slide2}>
+                        <TouchableOpacity style={styles.continue}>
+                            <Text style={styles.button_text}>English</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.slide3}>
+                        <TouchableOpacity style={styles.continue}>
+                            <Text style={styles.button_text}>Spanish</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.slide4}>
+                        <TouchableOpacity style={styles.continue}>
+                            <Text style={styles.button_text}>French</Text>
+                        </TouchableOpacity>
+                    </View>
+                </Swiper>
+                <View style={styles.button_container}>
+                    <TouchableOpacity style={styles.continue}>
+                        <Text style={styles.button_text}>Continue</Text>
+                    </TouchableOpacity>
+                </View>
+            </ImageBackground>
         </View>
     );
 }
@@ -43,8 +68,9 @@ export default function LanguageSelection () {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#003441',
-        Width: "2rem"
+    },
+    image: {
+        flex: 1,
     },
     text_box: {
         flexDirection: "row",
@@ -66,7 +92,8 @@ const styles = StyleSheet.create({
         marginLeft: 24,
         flexDirection: "row",
         flexWrap: "wrap",
-        width: 284
+        width: 284,
+        marginBottom: 20
     },
     worry: {
         fontFamily: 'Montserrat-Bold',
@@ -95,13 +122,12 @@ const styles = StyleSheet.create({
     button_container: {
         flexDirection: "column",
         justifyContent: "space-between",
-        alignItems: "flex-end",
-        marginRight: "10%",
-        marginTop: "5%",
-        gap: 15
+        alignItems: "center",
+        gap: 15,
+        paddingTop: 10
     },
-    sign_buttons: {
-        width: 181,
+    continue: {
+        width: 285,
         height: 57,
         borderRadius: 20,
         backgroundColor: '#BF8148',
@@ -114,12 +140,48 @@ const styles = StyleSheet.create({
         fontSize: 20,
         textAlign: 'center',
     },
-    sign_in: {
-        backgroundColor: '#BF8148',
+    swipper: {
+        flex: 0.7,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 30,
     },
-    sign_up: {
-        backgroundColor: 'transparent',
-        borderWidth: 2,
-        borderColor: '#BF8148',
+    wrapper: { },
+    slide1: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    slide2: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    slide3: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    slide4: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: '#FFFFFF',
+        fontSize: 30,
+        fontWeight: 'bold',
+        fontFamily: 'Montserrat-Bold',
+        textAlign: 'center',
+        verticalAlign: 'middle'
+    },
+    arrows: {
+        backgroundColor: 'transparent', 
+        flexDirection: 'column', 
+        position: 'absolute', 
+        justifyContent: 'space-between', 
+        alignItems: 'center',
+        color: '#FFFFFF',
+        verticalAlign: 'middle',
     }
 });
