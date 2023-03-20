5afdcae7eebe1980d870476aca3a203dc0179b8e
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-03-20 20:43:35 +0200

    feat: make localization for LanguageSelection

--- Localization.js
+++ Localization.js
@@ -1,30 +1,30 @@
 import memoize from 'lodash.memoize'; // Use for caching/memoize for better performance
 import { I18n } from "i18n-js";
-import * as Localization from 'expo-localization';
 import { I18nManager } from 'react-native';
-import en from './assets/locales/en.json'
 
 // make object
 const translator = new I18n();
 
+// to ge the tranlation
 const translationGetters = {
     en: () => require('./assets/locales/en.json'),
 };
-  
 
-// translate
+
+// translate is helper function to get the word with the current language
 export const translate = memoize(
     // including missing names
     (key, config) => translator.t(key, config).includes('missing') ? key : translator.t(key, config),
-    
+
     // cache
     (key, config) => (config ? key + JSON.stringify(config) : key),
 );
 
 // set the language
 export const init = () => {
-    const fallback = {languageTag: 'en', isRTL: false};
+    const fallback = { languageTag: 'en', isRTL: false };
 
+    // default language
     const { languageTag, isRTL } = fallback;
 
     // clear translation cache
@@ -34,6 +34,6 @@ export const init = () => {
     I18nManager.forceRTL(isRTL);
 
     // set i18n-js config
-    translator.translations = {[languageTag]: translationGetters[languageTag]()};
+    translator.translations = { [languageTag]: translationGetters[languageTag]() };
     translator.locale = languageTag;
 };
