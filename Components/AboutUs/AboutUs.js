e932e9793de91f327ae9e8a480f3bf88101a9f85
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-05-03 15:01:24 +0300

    refactor: adjust AboutUS

--- Components/AboutUs/AboutUs.js
+++ Components/AboutUs/AboutUs.js
@@ -1,29 +1,32 @@
 import { translate } from '../../Localization'
-import { View, Text, Image, ImageBackground, TouchableOpacity } from 'react-native';
+import { View, Text, Image, ImageBackground, TouchableOpacity, ScrollView, SafeAreaView} from 'react-native';
 import { styles } from './Styles';
 
 export default function AboutUs({ navigation }) {
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
             <ImageBackground source={require('../../assets/AboutUs/AboutUs.png')} style={styles.background}>
                 <TouchableOpacity onPress={() => navigation.goBack()}>
                     <Image source={require('../../assets/AboutUs/ep_close-bold.png')} style={styles.close} />
                 </TouchableOpacity>
                 <Text style={styles.copyright}>{translate('AboutUs.copyright')}</Text>
+                <View style={styles.line} />
                 <Text style={styles.description}>{translate('AboutUs.description')}</Text>
-                <View style={styles.usingAppicons}>
-                    <TouchableOpacity>
-                        <Image source={require('../../assets/register/google.png')} />
-                    </TouchableOpacity>
-                    <TouchableOpacity>
-                        <Image source={require('../../assets/register/twitter.png')} />
-                    </TouchableOpacity>
-                    <TouchableOpacity>
-                        <Image source={require('../../assets/register/facebook.png')} />
-                    </TouchableOpacity>
+                <View style={styles.contactus}>
+                    <View style={styles.usingAppicons}>
+                        <TouchableOpacity style={styles.IconButton}>
+                            <Image source={require('../../assets/register/google.png')} style={styles.icon}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.IconButton}>
+                            <Image source={require('../../assets/register/twitter.png')} style={styles.icon}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.IconButton}>
+                            <Image source={require('../../assets/register/facebook.png')} style={styles.icon}/>
+                        </TouchableOpacity>
+                    </View>
+                    <Text style={styles.findus}>{translate('AboutUs.findus')}</Text>
                 </View>
-                <Text style={styles.findus}>{translate('AboutUs.findus')}</Text>
             </ImageBackground>
-        </View>
+        </SafeAreaView>
     );
 }
--- Components/AboutUs/Styles.js
+++ Components/AboutUs/Styles.js
@@ -4,35 +4,44 @@ import { colors, fontFamily } from "../../AppStyles";
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: colors.DarkCyan,
     },
     background: {
         flex: 1,
-        resizeMode: "cover",
+        resizeMode: 'cover',
         width: "100%",
         height: "100%",
     },
+    line: {
+        borderBottomColor: colors.White,
+        borderBottomWidth: 2,
+        alignSelf: "center",
+        marginTop: "5%",
+        width: "40%",
+    },
     copyright: {
-        marginTop: "98%",
+        marginTop: "90%",
         alignSelf: "center",
         color: colors.White,
         fontSize: 13,
         fontFamily: fontFamily.MontserratRegular,
     },
     description: {
-        marginTop: "15%",
+        marginTop: "10%",
         alignSelf: "center",
         textAlign: "center",
         color: colors.White,
         fontSize: 15,
         fontFamily: fontFamily.MontserratRegular,
-        width: 315
+        width: "80%"
     },
     usingAppicons: {
         flexDirection: "row",
         justifyContent: "space-evenly",
-        width: 250,
+        width: "100%",
         alignSelf: "center",
-        marginTop: "8%"
+        marginTop: "-8%",
+        zIndex: 9999,
     },
     findus: {
         marginTop: "5%",
@@ -43,8 +52,26 @@ export const styles = StyleSheet.create({
     },
     close: {
         position: "absolute",
-        marginTop: 53,
         alignSelf: 'flex-end',
         right: 24,
+        top: 40,
+    },
+    contactus: {
+        borderColor: colors.White,
+        borderWidth: 2,
+        borderRadius: 20,
+        width: "80%",
+        alignSelf: "center",
+        marginTop: "15%",
+        paddingBottom: "50%",
+    },
+    icon: {
+        backgroundColor: colors.DarkCyan,
+    },
+    IconButton: {
+        backgroundColor: colors.DarkCyan,
+        paddingHorizontal: 8,
+        alignContent: "center",
+        justifyContent: "center",
     }
 });
